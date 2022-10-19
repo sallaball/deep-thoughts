5e59a835b42e9a39f033134ddde6a66889b60893
sallaball <sallaball1206@gmail.com>
2022-10-19 15:13:47 -0500

    lession 6 updates

--- deep-thoughts/client/src/pages/Profile.js
+++ deep-thoughts/client/src/pages/Profile.js
@@ -8,7 +8,13 @@ import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
+
+import ThoughtForm from '../components/ThoughtForm';
+
 const Profile = (props) => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -33,6 +39,16 @@ const Profile = (props) => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
@@ -40,6 +56,12 @@ const Profile = (props) => {
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
+
+      {userParam && (
+      <button className='btn ml-auto' onClick={handleClick}>
+        Add Friend
+      </button>
+      )}
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
